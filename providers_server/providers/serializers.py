3f8203e50bcd1ac22a7a534a75abb034740e5818
Jose Chu <jchudb93@gmail.com>
2019-05-09 22:52:14 -0500

    serializers>

--- providers_server/providers/serializers.py
+++ providers_server/providers/serializers.py
@@ -1,4 +1,5 @@
 from .models import *
+from rest_framework import serializers
 
 
 class StrategicGoalSerializer(serializers.ModelSerializer):    
--- providers_server/providers_server/settings.py
+++ providers_server/providers_server/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
-    'providers_servers.providers'
+    'rest_framework',
+    'providers'
 ]
 
 MIDDLEWARE = [
