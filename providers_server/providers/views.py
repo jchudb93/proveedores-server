6f941aa1a13c585e35b62e0ead54b741c2b0988f
Jose Chu <jchudb93@gmail.com>
2019-06-19 23:15:44 -0500

    provideres in contract

--- providers_server/providers/views.py
+++ providers_server/providers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
@@ -43,4 +44,20 @@ class ProviderView(viewsets.ModelViewSet):
 class SupplierView(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
+    
 
+class UpdateContractState(generics.UpdateAPIView):
+    
+    queryset = Contract.objects.all()
+    serializer_class = ContractStateSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.state = request.data.get('state')
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
